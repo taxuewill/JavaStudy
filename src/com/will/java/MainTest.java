38beafb0df8699ce1c8677239bfb65eeda57f9df
jing wang2 <jing.wang2@ninebot.com>
2019-10-29 09:19:49 +0800

    append

--- src/com/will/java/MainTest.java
+++ src/com/will/java/MainTest.java
@@ -4,6 +4,9 @@ public class MainTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello,Java!");
+		Thread t = new Thread(()->{
+			System.out.println("I'm a new thread");
+		});
 	}
 
 }
